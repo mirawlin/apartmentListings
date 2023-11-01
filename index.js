5d2b854b91a7d951ff8b9a628b936f87de7e6892
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-11-01 18:07:21 +0100

    create hapi server

---
+++ index.js
@@ -0,0 +1,36 @@
+import Hapi from '@hapi/hapi';
+import {routes} from './src/limi/routes.js'
+import {register, name} from './src/limi/application/index.js'
+
+'use strict';
+
+const createServer = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    await setupRoutesAndPlugins(server);
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+const setupRoutesAndPlugins = async function (server) {
+    const configuration = {
+        plugin: {
+            register,
+            name, // Use the name from the imported module
+        },
+    }
+
+    await server.register(configuration);
+};
+
+createServer();
