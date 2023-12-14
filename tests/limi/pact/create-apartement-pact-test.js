15a973bd32b1a73087f7fcae7f28f3fcec3d16f2
miranda.lin-guignard <miranda.w.lin@gmail.com>
2023-12-14 22:37:54 +0100

    dynamically add provider version using git hash

--- tests/limi/pact/create-apartement-pact-test.js
+++ tests/limi/pact/create-apartement-pact-test.js
@@ -1,9 +1,11 @@
 import {MessageProviderPact, providerWithMetadata} from '@pact-foundation/pact';
+import {execSync} from 'child_process';
+import {Apartment} from "../../../src/limi/domain/model/apartment.js";
 
 const createApartmentClient = {
     createApartment: () => {
         return new Promise((resolve, reject) => {
-            resolve({
+            resolve( new Apartment ({
                 id: "fc02a23a-c711-45ab-97c4-00c00c327fd2",
                 name: "Apartment name",
                 description: "large T4",
@@ -11,22 +13,29 @@ const createApartmentClient = {
                 city: "Paris",
                 postcode: 75001,
                 energyRating: "E",
-            })
+            }))
         })
     },
 }
+
+const exec = command =>
+    execSync(command)
+        .toString()
+        .trim()
+
 describe('Pact Verification', () => {
     describe("Message provider tests", () => {
         // 2 Pact setup
         const p = new MessageProviderPact({
             messageProviders: {
+                // Name should match the 'expects to receive' in the consumer pact
                 'New Apartment created': providerWithMetadata(
                     () => createApartmentClient.createApartment(), {
                     queue: 'apartments-created',
                 }),
             },
             provider: "apartment-listings",
-            providerVersion: "1.0.0",
+            providerVersion: exec('git rev-parse HEAD'),
             pactBrokerUrl: 'http://localhost:9292',
             consumerVersionTags: ['latest'],
             publishVerificationResult: true,
